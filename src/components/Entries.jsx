4fcdc5fc842fcd0dd828890aa076bb656f8a95c3
Rita Bradley <rita@ritabradley.dev>
2023-03-22 20:49:12 -0700

    :sparkles: :construction: add `Entries` component
    - destructured `name` and `numOfEntries` with default values until state and backend is set up
    - TODO: change prop values with data from state/database

--- src/components/Entries.jsx
+++ src/components/Entries.jsx
@@ -1,3 +1,12 @@
-const Entries = () => {};
+const Entries = ({ name = "Rita", numOfEntries = 5 }) => {
+  return (
+    <div className="text-violet-600 text-xl md:text-2xl my-8">
+      {name}, your current entry count is...{" "}
+      <span className="font-bold text-3xl md:text-4xl text-white">
+        {numOfEntries}
+      </span>
+    </div>
+  );
+};
 
 export default Entries;
