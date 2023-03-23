2850932f5a557cde44e07fbbd550b282f22bea06
Rita Bradley <rita@ritabradley.dev>
2023-03-22 18:50:58 -0700

    :sparkles: use `react-parallax-tilt` to create glare logo w/ tilt

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,3 +1,21 @@
-const Logo = () => {}
+import Tilt from "react-parallax-tilt";
+import logo from "../assets/face-detective-icon.png";
 
-export default Logo
+const Logo = () => {
+  return (
+    <Tilt
+      className="rounded-md shadow-lg flex justify-center items-center bg-reverse-gradient"
+      perspective={500}
+      glareEnable={true}
+      glareMaxOpacity={0.45}
+      scale={1.02}
+      style={{ height: 150, width: 150 }}
+    >
+      <div className="m-4 mt-0">
+        <img src={logo} alt="Face Detective logo" />
+      </div>
+    </Tilt>
+  );
+};
+
+export default Logo;
