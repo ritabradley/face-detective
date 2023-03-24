95e17dcf0a8fb5ad61b095d4b22a7d045bce0848
Rita Bradley <rita@ritabradley.dev>
2023-03-24 16:13:18 -0700

    :sparkles: create sign-in form

--- src/App.jsx
+++ src/App.jsx
@@ -1,102 +1,111 @@
-import {useState} from "react";
+import { useState } from "react";
 import Particles from "particles-bg";
+import SignIn from "./components/SignIn";
 import Navigation from "./components/Navigation";
 import Logo from "./components/Logo";
 import ImageForm from "./components/ImageForm";
 import Entries from "./components/Entries";
 import FaceDetection from "./components/FaceDetection";
 
-
 const App = () => {
-	const [input, setInput] = useState("");
-	const [imgUrl, setImgUrl] = useState("");
-	const [faceData, setFaceData] = useState([]);
+  const [input, setInput] = useState("");
+  const [imgUrl, setImgUrl] = useState("");
+  const [faceData, setFaceData] = useState([]);
 
-	const handleInputChange = (e) => {
-		setInput(e.target.value);
-	};
+  const handleInputChange = (e) => {
+    setInput(e.target.value);
+  };
 
-	const handleFaceData = (data) => {
-		setFaceData(data);
-	};
+  const handleFaceData = (data) => {
+    setFaceData(data);
+  };
 
-	const handleSubmit = (e) => {
-		e.preventDefault();
-		setImgUrl(input);
-		const PAT = "d04f663f574948a890d313e514794d6f";
-		const USER_ID = "ritalbradley";
-		const APP_ID = "face-detective";
-		const MODEL_ID = "face-detection";
-		const MODEL_VERSION_ID = "6dc7e46bc9124c5c8824be4822abe105";
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setImgUrl(input);
+    const PAT = "d04f663f574948a890d313e514794d6f";
+    const USER_ID = "ritalbradley";
+    const APP_ID = "face-detective";
+    const MODEL_ID = "face-detection";
+    const MODEL_VERSION_ID = "6dc7e46bc9124c5c8824be4822abe105";
 
-		const raw = JSON.stringify({
-			"user_app_id": {
-				"user_id": USER_ID,
-				"app_id": APP_ID
-			},
-			"inputs": [
-				{
-					"data": {
-						"image": {
-							"url": input
-						}
-					}
-				}
-			]
-		});
+    const raw = JSON.stringify({
+      user_app_id: {
+        user_id: USER_ID,
+        app_id: APP_ID,
+      },
+      inputs: [
+        {
+          data: {
+            image: {
+              url: input,
+            },
+          },
+        },
+      ],
+    });
 
-		const requestOptions = {
-			method: "POST",
-			headers: {
-				"Accept": "application/json",
-				"Authorization": "Key " + PAT
-			},
-			body: raw
-		};
-		fetch("https://api.clarifai.com/v2/users/" + USER_ID + "/apps/" + APP_ID + "/models/" + MODEL_ID + "/outputs", requestOptions)
-			.then(response => response.json())
-			.then(result => {
-				const faceRegions = result.outputs[0].data.regions;
-				const imageElement = document.getElementById("input-image");
-				const width = Number(imageElement.width);
-				const height = Number(imageElement.height);
-				const faceData = faceRegions.map(region => {
-					const boundingBox = region.region_info.bounding_box;
-					return {
-						topRow: boundingBox.top_row * height,
-						rightCol: width - boundingBox.right_col * width,
-						bottomRow: height - boundingBox.bottom_row * height,
-						leftCol: boundingBox.left_col * width,
-					};
-				});
-				handleFaceData(faceData);
-			})
-			.catch(error => console.log("error", error));
-	};
+    const requestOptions = {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        Authorization: "Key " + PAT,
+      },
+      body: raw,
+    };
+    fetch(
+      "https://api.clarifai.com/v2/users/" +
+        USER_ID +
+        "/apps/" +
+        APP_ID +
+        "/models/" +
+        MODEL_ID +
+        "/outputs",
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) => {
+        const faceRegions = result.outputs[0].data.regions;
+        const imageElement = document.getElementById("input-image");
+        const width = Number(imageElement.width);
+        const height = Number(imageElement.height);
+        const faceData = faceRegions.map((region) => {
+          const boundingBox = region.region_info.bounding_box;
+          return {
+            topRow: boundingBox.top_row * height,
+            rightCol: width - boundingBox.right_col * width,
+            bottomRow: height - boundingBox.bottom_row * height,
+            leftCol: boundingBox.left_col * width,
+          };
+        });
+        handleFaceData(faceData);
+      })
+      .catch((error) => console.log("error", error));
+  };
 
-	return (
-		<div>
-			<Particles
-				className="min-h-screen"
-				color="#ffffff"
-				num={250}
-				type="cobweb"
-				bg={true}
-			/>
-			<h1>Face Detective</h1>
-			<Navigation/>
-			<Logo/>
-			<div
-				className="grid place-content-center text-center w-full max-w-4xl mt-6 mx-auto">
-				<Entries/>
-				<ImageForm
-					onInputChange={handleInputChange}
-					onButtonSubmit={handleSubmit}
-				/>
-				<FaceDetection imgUrl={imgUrl} faceData={faceData}/>
-			</div>
-		</div>
-	);
+  return (
+    <div>
+      <Particles
+        className="min-h-screen"
+        color="#ffffff"
+        num={250}
+        type="cobweb"
+        bg={true}
+      />
+      <h1>Face Detective</h1>
+      <SignIn />
+      <Navigation />
+      <Logo />
+      <div className="grid place-content-center text-center w-full max-w-4xl mt-6 mx-auto">
+        <Entries />
+        <ImageForm
+          onInputChange={handleInputChange}
+          onButtonSubmit={handleSubmit}
+        />
+        <FaceDetection imgUrl={imgUrl} faceData={faceData} />
+      </div>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/SignIn.jsx
@@ -0,0 +1,95 @@
+// TODO: We're going to use some OAuth magic to make the sign in process easier as well
+// TODO: Have a 'Sign In with Google' button
+// TODO: Have a 'Sign In with Facebook' button
+// TODO: Have a 'Sign In with Twitter' button
+
+const SignIn = () => {
+  return (
+    <div className="flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-8">
+      <div className="sm:mx-auto sm:w-full sm:max-w-md">
+        <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-violet-700">
+          Sign in to your account
+        </h2>
+        <p className="mt-2 text-center text-md text-white">
+          Or{" "}
+          <a
+            href="#"
+            className="font-bold text-violet-500 hover:text-violet-600"
+          >
+            sign up now
+          </a>
+        </p>
+      </div>
+      <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+        <div className="input-bg-pattern py-8 px-4 shadow sm:rounded-lg sm:px-10">
+          <form className="space-y-6" action="#" method="POST">
+            <div>
+              <label htmlFor="email" className="form-label">
+                Email address
+              </label>
+              <div className="mt-2">
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  autoComplete="email"
+                  required
+                  className="signin-form-input"
+                />
+              </div>
+            </div>
+
+            <div>
+              <label htmlFor="password" className="form-label">
+                Password
+              </label>
+              <div className="mt-2">
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  autoComplete="current-password"
+                  required
+                  className="signin-form-input"
+                />
+              </div>
+            </div>
+            <div className="flex items-center justify-between">
+              <div className="flex items-center">
+                <input
+                  id="remember-me"
+                  name="remember-me"
+                  type="checkbox"
+                  className="h-4 w-4 rounded border-violet-300 text-violet-500 focus:ring-violet-500"
+                />
+                <label
+                  htmlFor="remember-me"
+                  className="ml-2 block text-sm text-violet-700"
+                >
+                  Remember me
+                </label>
+              </div>
+
+              <div className="text-sm">
+                <a
+                  href="#"
+                  className="font-medium text-violet-500 hover:text-violet-600"
+                >
+                  Forgot your password?
+                </a>
+              </div>
+            </div>
+
+            <div>
+              <button type="submit" className="signin-button">
+                Sign in
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SignIn;
