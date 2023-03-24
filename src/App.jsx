33fb01b02f258125309dac06ecfd54c83d3a8d3e
Rita Bradley <rita@ritabradley.dev>
2023-03-23 17:03:29 -0700

    :sparkles: display the image and call the API

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,82 @@
-import { useState } from "react";
+import {useState} from "react";
 import Particles from "particles-bg";
 import Navigation from "./components/Navigation";
 import Logo from "./components/Logo";
 import ImageForm from "./components/ImageForm";
 import Entries from "./components/Entries";
+import FaceDetection from "./components/FaceDetection";
+
 
 const App = () => {
-  const [imgUrl, setImgUrl] = useState("");
+	const [input, setInput] = useState("");
+	const [imgUrl, setImgUrl] = useState("");
 
-  const handleInputChange = (e) => {
-    console.log(e.target.value);
-    setImgUrl(e.target.value);
-  };
+	const handleInputChange = (e) => {
+		setInput(e.target.value);
+	};
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(imgUrl);
-  };
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		setImgUrl(input);
+		const PAT = "d04f663f574948a890d313e514794d6f";
+		const USER_ID = "ritalbradley";
+		const APP_ID = "face-detective";
+		const MODEL_ID = "face-detection";
+		const MODEL_VERSION_ID = "6dc7e46bc9124c5c8824be4822abe105";
 
-  return (
-    <div>
-      <Particles
-        className="min-h-screen"
-        color="#ffffff"
-        num={250}
-        type="cobweb"
-        bg={true}
-      />
-      <h1>Face Detective</h1>
-      <Navigation />
-      <Logo />
-      <div className="grid place-content-center w-full m-auto mt-4 text-center">
-        <Entries />
-        <ImageForm
-          onInputChange={handleInputChange}
-          onButtonSubmit={handleSubmit}
-        />
-      </div>
-    </div>
-  );
+		const raw = JSON.stringify({
+			"user_app_id": {
+				"user_id": USER_ID,
+				"app_id": APP_ID
+			},
+			"inputs": [
+				{
+					"data": {
+						"image": {
+							"url": input
+						}
+					}
+				}
+			]
+		});
+
+		const requestOptions = {
+			method: "POST",
+			headers: {
+				"Accept": "application/json",
+				"Authorization": "Key " + PAT
+			},
+			body: raw
+		};
+		fetch("https://api.clarifai.com/v2/users/" + USER_ID + "/apps/" + APP_ID + "/models/" + MODEL_ID + "/outputs", requestOptions)
+			.then(response => response.json())
+			.then(result => console.log(result))
+			.catch(error => console.log("error", error));
+	};
+
+	return (
+		<div>
+			<Particles
+				className="min-h-screen"
+				color="#ffffff"
+				num={250}
+				type="cobweb"
+				bg={true}
+			/>
+			<h1>Face Detective</h1>
+			<Navigation/>
+			<Logo/>
+			<div
+				className="grid place-content-center w-full m-auto mt-4 text-center">
+				<Entries/>
+				<ImageForm
+					onInputChange={handleInputChange}
+					onButtonSubmit={handleSubmit}
+				/>
+				<FaceDetection imgUrl={imgUrl}/>
+			</div>
+		</div>
+	);
 };
 
 export default App;
--- src/components/FaceDetection.jsx
+++ src/components/FaceDetection.jsx
@@ -1,3 +1,13 @@
-const FaceDetection = () => {};
+const FaceDetection = ({imgUrl}) => {
+	return (
+		<div className="flex justify-center">
+			{imgUrl ?
+				<img src={imgUrl} alt="face" width="100%" height="auto"/> :
+				<p>No image to display</p>
+			}
+
+		</div>
+	);
+};
 
 export default FaceDetection;
