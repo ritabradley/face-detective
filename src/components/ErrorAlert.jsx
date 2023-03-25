268d37003ce185fb448b5bf032bed049ad20ae5a
Rita Bradley <rita@ritabradley.dev>
2023-03-24 19:39:11 -0700

    :sparkles: create an error notification for unfilled out form inputs

---
+++ src/components/ErrorAlert.jsx
@@ -0,0 +1,18 @@
+import { XCircleIcon } from "@heroicons/react/20/solid";
+
+const ErrorAlert = ({ alertMessage }) => {
+  return (
+    <div className="rounded-md bg-red-50 p-4">
+      <div className="flex">
+        <div className="flex-shrink-0">
+          <XCircleIcon className="h-5 w-5 text-red-400" aria-hidden="true" />
+        </div>
+        <div className="ml-3">
+          <div className="text-sm text-red-700">{alertMessage}</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ErrorAlert;
