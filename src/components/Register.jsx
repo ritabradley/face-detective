c56cda1c6ce230b9836fab7ce040ea26a8777e49
Rita Bradley <rita@ritabradley.dev>
2023-03-26 15:35:57 -0700

    :safety_vest: check if email already exists
    - if entered email exists, prompt users to sign in or reset password

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,194 +1,238 @@
-import { useState } from "react";
+import {useState} from "react";
 import ErrorAlert from "./ErrorAlert.jsx";
 
-const Register = ({ onRouteChange }) => {
-  const [firstName, setFirstName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [errorMessage, setErrorMessage] = useState("");
-  const [firstNameError, setFirstNameError] = useState(false);
-  const [emailError, setEmailError] = useState(false);
-  const [passwordError, setPasswordError] = useState(false);
+const Register = ({onRouteChange}) => {
+	const [firstName, setFirstName] = useState("");
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+	const [errorMessage, setErrorMessage] = useState("");
+	const [firstNameError, setFirstNameError] = useState(false);
+	const [emailError, setEmailError] = useState(false);
+	const [passwordError, setPasswordError] = useState(false);
 
-  const isEmailValid = (email) => {
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return emailRegex.test(email);
-  };
+	const isEmailValid = (email) => {
+		const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+		return emailRegex.test(email);
+	};
 
-  const isPasswordValid = (password) => {
-    const passwordRegex =
-      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[~`!@#$%^&*()_+\-={[}\]|\\:;"'<,>.?/])[A-Za-z\d~`!@#$%^&*()_+\-={[}\]|\\:;"'<,>.?/]{8,}$/;
-    return passwordRegex.test(password);
-  };
+	const isPasswordValid = (password) => {
+		const passwordRegex =
+			/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[~`!@#$%^&*()_+\-={[}\]|\\:;"'<,>.?/])[A-Za-z\d~`!@#$%^&*()_+\-={[}\]|\\:;"'<,>.?/]{8,}$/;
+		return passwordRegex.test(password);
+	};
 
-  const handleRegistration = async (name, email, password) => {
-    try {
-      const response = await fetch('http://localhost:3000/register', {
-        method: 'POST',
-        headers: {
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-          name,
-          email,
-          password
-        })
-      })
+	const handleRegistration = async (name, email, password) => {
+		try {
+			// Check if the email already exists
+			const checkEmailResponse = await fetch("http://localhost:3000/check-email", {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json"
+				},
+				body: JSON.stringify({email})
+			});
 
-      const data = await response.json()
+			const checkEmailData = await checkEmailResponse.json();
 
-      if (response.ok) {
-        return data
-      } else {
-        throw new Error(data)
-      }
-    } catch (err) {
-      console.error("Error register your account", err)
-    }
-  }
+			if (!checkEmailResponse.ok) {
+				throw new Error(checkEmailData.message);
+			}
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    let hasError = false;
-    let errorMessages = [];
+			// If the email doesn't exist, continue with registration
+			const registerResponse = await fetch("http://localhost:3000/register", {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json"
+				},
+				body: JSON.stringify({
+					name,
+					email,
+					password
+				})
+			});
 
-    if (firstName.trim() === "") {
-      setFirstNameError(true);
-      hasError = true;
-      errorMessages.push("First Name is required.");
-    } else {
-      setFirstNameError(false);
-    }
+			const registerData = await registerResponse.json();
 
-    if (email.trim() === "" || !isEmailValid(email)) {
-      setEmailError(true);
-      hasError = true;
-      errorMessages.push("Please enter a valid email address.");
-    } else {
-      setEmailError(false);
-    }
-
-    if (password.trim() === "" || !isPasswordValid(password)) {
-      setPasswordError(true);
-      hasError = true;
-      errorMessages.push(
-        "Password must be at least 8 characters long, contain numbers, lowercase and uppercase letters, and any of the following special characters: ~`! @#$%^&*()_-+={[}]|:;\"'<,>.?/."
-      );
-    } else {
-      setPasswordError(false);
-    }
-
-    if (hasError) {
-      setErrorMessage(errorMessages.join(" "));
-      return;
-    }
-
-    const registrationResult = await handleRegistration(firstName, email, password)
-
-    if (registrationResult) {
-      onRouteChange("home");
-    } else {
-      setErrorMessage("Something went wrong. Please try again.")
-    }
+			if (registerResponse.ok) {
+				return registerData;
+			} else {
+				throw new Error(registerData.message);
+			}
+		} catch (err) {
+			console.error("Error registering your account", err);
+			throw err;
+		}
+	};
 
 
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		let hasError = false;
+		let errorMessages = [];
 
-  };
+		if (firstName.trim() === "") {
+			setFirstNameError(true);
+			hasError = true;
+			errorMessages.push("First Name is required.");
+		} else {
+			setFirstNameError(false);
+		}
 
-  return (
-    <div className="flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-8">
-      <div className="sm:mx-auto sm:w-full sm:max-w-md">
-        <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-violet-700">
-          Register for an account
-        </h2>
-        <p className="mt-2 text-center text-md text-white">
-          Or{" "}
-          <a
-            href="#"
-            className="font-bold text-violet-500 hover:text-violet-600"
-            onClick={() => onRouteChange("signin")}
-          >
-            sign in
-          </a>
-        </p>
-      </div>
-      <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
-        <div className="input-bg-pattern py-8 px-4 shadow sm:rounded-lg sm:px-10">
-          <form className="space-y-6" action="#" method="POST">
-            <div>
-              <label htmlFor="first-name" className="form-label">
-                First Name
-              </label>
-              <div className="mt-2">
-                <input
-                  id="first-name"
-                  name="first-name"
-                  type="text"
-                  autoComplete="given-name"
-                  required
-                  className={`signin-form-input ${
-                    firstNameError ? "border-red-500" : ""
-                  }`}
-                  value={firstName}
-                  onChange={(e) => setFirstName(e.target.value)}
-                />
-              </div>
-            </div>
+		if (email.trim() === "" || !isEmailValid(email)) {
+			setEmailError(true);
+			hasError = true;
+			errorMessages.push("Please enter a valid email address.");
+		} else {
+			setEmailError(false);
+		}
 
-            <div>
-              <label htmlFor="email" className="form-label">
-                Email address
-              </label>
-              <div className="mt-2">
-                <input
-                  id="email"
-                  name="email"
-                  type="email"
-                  autoComplete="email"
-                  required
-                  className={`signin-form-input ${
-                    emailError ? "border-red-500" : ""
-                  }`}
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                />
-              </div>
-            </div>
+		if (password.trim() === "" || !isPasswordValid(password)) {
+			setPasswordError(true);
+			hasError = true;
+			errorMessages.push(
+				"Password must be at least 8 characters long, contain numbers, lowercase and uppercase letters, and any of the following special characters: ~`! @#$%^&*()_-+={[}]|:;\"'<,>.?/."
+			);
+		} else {
+			setPasswordError(false);
+		}
 
-            <div>
-              <label htmlFor="password" className="form-label">
-                Password
-              </label>
-              <div className="mt-2">
-                <input
-                  id="password"
-                  name="password"
-                  type="password"
-                  autoComplete="new-password"
-                  required
-                  className={`signin-form-input ${
-                    passwordError ? "border-red-500" : ""
-                  }`}
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                />
-              </div>
-            </div>
-            {errorMessage && <ErrorAlert alertMessage={errorMessage} />}
-            <div>
-              <button
-                onClick={handleSubmit}
-                type="submit"
-                className="signin-button"
-              >
-                Register
-              </button>
-            </div>
-          </form>
-        </div>
-      </div>
-    </div>
-  );
+		if (hasError) {
+			setErrorMessage(errorMessages.join(" "));
+			return;
+		}
+
+		try {
+			const registrationResult = await handleRegistration(firstName, email, password);
+
+			if (registrationResult) {
+				onRouteChange("home");
+			} else {
+				setErrorMessage("Something went wrong. Please try again.");
+			}
+		} catch (err) {
+			if (err.message === "Email already exists") {
+				setErrorMessage(
+					<>
+						The email is already registered. Please{" "}
+						<span
+							className="text-violet-500 hover:text-violet-600 cursor-pointer"
+							onClick={() => onRouteChange("signin")}
+						>
+      sign in.
+    </span>{" "}
+						If you forgot your password,{" "}
+						<span
+							className="text-violet-500 hover:text-violet-600 cursor-pointer"
+							onClick={() => onRouteChange("forgotpassword")}
+						>
+      click here
+    </span>{" "}
+						to reset it.
+					</>
+				);
+			} else {
+				setErrorMessage("Something went wrong. Please try again.");
+			}
+		}
+	};
+
+	return (
+		<div
+			className="flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-8">
+			<div className="sm:mx-auto sm:w-full sm:max-w-md">
+				<h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-violet-700">
+					Register for an account
+				</h2>
+				<p className="mt-2 text-center text-md text-white">
+					Or{" "}
+					<a
+						href="#"
+						className="font-bold text-violet-500 hover:text-violet-600"
+						onClick={() => onRouteChange("signin")}
+					>
+						sign in
+					</a>
+				</p>
+			</div>
+			<div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+				<div
+					className="input-bg-pattern py-8 px-4 shadow sm:rounded-lg sm:px-10">
+					<form className="space-y-6" action="#" method="POST">
+						<div>
+							<label htmlFor="first-name" className="form-label">
+								First Name
+							</label>
+							<div className="mt-2">
+								<input
+									id="first-name"
+									name="first-name"
+									type="text"
+									autoComplete="given-name"
+									required
+									className={`signin-form-input ${
+										firstNameError ? "border-red-500" : ""
+									}`}
+									value={firstName}
+									onChange={(e) => setFirstName(e.target.value)}
+								/>
+							</div>
+						</div>
+
+						<div>
+							<label htmlFor="email" className="form-label">
+								Email address
+							</label>
+							<div className="mt-2">
+								<input
+									id="email"
+									name="email"
+									type="email"
+									autoComplete="email"
+									required
+									className={`signin-form-input ${
+										emailError ? "border-red-500" : ""
+									}`}
+									value={email}
+									onChange={(e) => setEmail(e.target.value)}
+								/>
+							</div>
+						</div>
+
+						<div>
+							<label htmlFor="password" className="form-label">
+								Password
+							</label>
+							<div className="mt-2">
+								<input
+									id="password"
+									name="password"
+									type="password"
+									autoComplete="new-password"
+									required
+									className={`signin-form-input ${
+										passwordError ? "border-red-500" : ""
+									}`}
+									value={password}
+									onChange={(e) => setPassword(e.target.value)}
+								/>
+							</div>
+						</div>
+						{errorMessage &&
+							<ErrorAlert alertMessage={errorMessage}/>}
+						<div>
+							<button
+								onClick={handleSubmit}
+								type="submit"
+								className="signin-button"
+							>
+								Register
+							</button>
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Register;
