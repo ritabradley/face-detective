8401b2997dadf9a19db2fa8e1d3e61a11ed9050a
Rita Bradley <rita@ritabradley.dev>
2023-03-26 00:38:54 -0700

    :bug: bugfix: fix sign out button still showing up on Sign In page after signing out

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,29 +1,29 @@
-const Navigation = ({ onRouteChange, isSignedIn }) => {
-  return isSignedIn ? (
-    <nav className="flex justify-end">
-      <a
-        onClick={() => onRouteChange("signin")}
-        className="cursor-pointer p-3 text-xl text-violet-700 underline hover:text-violet-500"
-      >
-        Sign Out
-      </a>
-    </nav>
-  ) : (
-    <nav className="flex justify-end">
-      <a
-        onClick={() => onRouteChange("signin")}
-        className="cursor-pointer p-3 text-xl text-violet-700 underline hover:text-violet-500"
-      >
-        Sign In
-      </a>
-      <a
-        onClick={() => onRouteChange("register")}
-        className="cursor-pointer p-3 text-xl text-violet-700 underline hover:text-violet-500"
-      >
-        Register
-      </a>
-    </nav>
-  );
+const Navigation = ({onRouteChange, isSignedIn}) => {
+	return isSignedIn ? (
+		<nav className="flex justify-end">
+			<a
+				onClick={() => onRouteChange("signout")}
+				className="cursor-pointer p-3 text-xl text-violet-700 underline hover:text-violet-500"
+			>
+				Sign Out
+			</a>
+		</nav>
+	) : (
+		<nav className="flex justify-end">
+			<a
+				onClick={() => onRouteChange("signin")}
+				className="cursor-pointer p-3 text-xl text-violet-700 underline hover:text-violet-500"
+			>
+				Sign In
+			</a>
+			<a
+				onClick={() => onRouteChange("register")}
+				className="cursor-pointer p-3 text-xl text-violet-700 underline hover:text-violet-500"
+			>
+				Register
+			</a>
+		</nav>
+	);
 };
 
 export default Navigation;
